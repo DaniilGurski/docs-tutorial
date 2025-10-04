3e24360e713cd13ad1096edb793172131ed1a926
Daniil Gurski <daniilgurski6@gmail.com>
2025-10-04 12:08:48 +0200

    add extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,14 @@
 import React from "react";
 import { StarterKit } from "@tiptap/starter-kit";
 import { useEditor, EditorContent } from "@tiptap/react";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 
 export function Editor() {
   const editor = useEditor({
@@ -13,16 +21,26 @@ export function Editor() {
           "focus:outline-none print:border-0 bg-white border-[#C7C7C7] border-2 flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World!</p>",
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({ nested: true }),
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      Image,
+      ImageResize,
+    ],
+    content: ``,
   });
 
   {
-    /* TODO: Try removing overflow-x-auto and print:overflow-visable to see what happens */
+    /* TODO: Try removing overflow-x-auto and print:overflow-visable to see s happens */
   }
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
-      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 print:min-w-0 mx-auto">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 print:min-w-0 mx-auto ">
         <EditorContent editor={editor} />
       </div>
     </div>
